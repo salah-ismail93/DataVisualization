1fcb49ceb4a1fcb2ad9c2df47ed8da6d1f2cd82e
Salah Ismail <S5239380@studenti.unige.it>
2023-11-15 01:05:36 +0100

    start splitting the big chunk code into components

---
+++ src/components/HeatMap.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'HeatMap',
+    mounted() {
+
+        
+    },
+
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/StackedBarChart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'StackedBarChart',
+    mounted() {
+
+        
+    },
+
+}
+</script>
+
+<style>
+
+
+</style>
