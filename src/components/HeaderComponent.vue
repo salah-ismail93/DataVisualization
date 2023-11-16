19a8db7979eb437e8409960f26f97b58fd2df03d
Salah Ismail <S5239380@studenti.unige.it>
2023-11-16 23:50:59 +0100

    fix the header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,78 +1,63 @@
 <template>
     <header class="bg-white">
-        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div class="flex lg:flex-1">
-                <router-link to="/">Husam Jenidi and Salah Ismail</router-link>
-            </div>
-
-             <!-- side bar-->
-            <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                        aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </button>
-            </div>
-            <div class="hidden lg:flex lg:gap-x-12">
-                <router-link class="text-sm font-semibold leading-6 text-gray-900" to="/AssignmentOne">Assignment 1</router-link>
-                <router-link class="text-sm font-semibold leading-6 text-gray-900" to="/AssignmentTwo">Assignment 2</router-link>
-                <a href="https://github.com/salah-ismail93/Datavisualization" target="_blank" class="text-sm font-semibold leading-6 text-gray-900"> Github <span aria-hidden="true">&rarr;</span></a>
-                <a href="https://colab.research.google.com/drive/1vY9JxPZKnyzf3IUrQeLZjeftFMdwIrE-#scrollTo=yqYbJfV08k4V" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Data processing and manipulation<span aria-hidden="true">&rarr;</span></a>
-            </div>
-        </nav>
-        <!-- Mobile menu, show/hide based on menu open state. -->
-        <div class="lg:hidden" v-if="isSidebarOpen" role="dialog" aria-modal="true">
-            <div
-                class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                <div class="flex items-center justify-between">
-                    <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="toggleSidebar">
-                        <span class="sr-only">Close menu</span>
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                            aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="mt-6 flow-root">
-                    <div class="-my-6 divide-y divide-gray-500/10">
-                        <div class="space-y-2 py-6">
-                            <router-link class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/AssignmentOne">Assignment 1</router-link>
-                        </div>
-                        <div class="space-y-2 py-6">
-                            <router-link class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/AssignmentTwo">Assignment 2</router-link>
-                        </div>
-                        <div class="py-6">
-                            <a href="https://github.com/salah-ismail93/Datavisualization"
-                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Github</a>
-                        </div>
-                        <div class="py-6">
-                            <a href="https://colab.research.google.com/drive/1vY9JxPZKnyzf3IUrQeLZjeftFMdwIrE-#scrollTo=yqYbJfV08k4V"
-                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Data processing and manipulation</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+          <router-link to="/">Husam Jenidi and Salah Ismail</router-link>
         </div>
+        <div class="flex lg:hidden">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <router-link class="text-sm font-semibold leading-6 text-gray-900" to="/AssignmentOne">Assignment 1</router-link>
+          <router-link class="text-sm font-semibold leading-6 text-gray-900" to="/AssignmentTwo">Assignment 2</router-link>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12 lg:flex-1 lg:justify-end">
+            <a href="https://github.com/salah-ismail93/Datavisualization" target="_blank" class="text-sm font-semibold leading-6 text-gray-900"> Github <span aria-hidden="true">&rarr;</span></a>
+            <a href="https://colab.research.google.com/drive/1vY9JxPZKnyzf3IUrQeLZjeftFMdwIrE-#scrollTo=yqYbJfV08k4V" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Colab<span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+      <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-10" />
+        <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div class="flex items-center justify-between">
+            <router-link to="/">Husam Jenidi and Salah Ismail</router-link>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                  <router-link class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/AssignmentOne">Assignment 1</router-link>
+              </div>
+              <div class="space-y-2 py-6">
+                  <router-link class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/AssignmentTwo">Assignment 2</router-link>
+              </div>
+              <div class="py-6">
+                  <a href="https://github.com/salah-ismail93/Datavisualization"
+                      class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Github</a>
+              </div>
+              <div class="py-6">
+                  <a href="https://colab.research.google.com/drive/1vY9JxPZKnyzf3IUrQeLZjeftFMdwIrE-#scrollTo=yqYbJfV08k4V"
+                      class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Data processing and manipulation</a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
     </header>
-</template>
+  </template>
 
-<script>
-export default {
-    name: 'HeaderComponent',
-    data() {
-        return {
-            isSidebarOpen: false,  // Tracks the state of the sidebar
-        };
-    },
-    methods: {
-        toggleSidebar() {
-            this.isSidebarOpen = !this.isSidebarOpen;  // Toggles the state of the sidebar
-        },
-    },
-}
+<script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const mobileMenuOpen = ref(false)
 
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,8 @@ module.exports = {
     screens:{
       sm:"640px",
       md:"768px",
+      lg: '960px',
+      xl: '1280px',
     }
   },
   plugins: [],
